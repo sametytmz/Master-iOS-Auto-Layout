9c46602ec6da143ebdcab75243f67d9bfeefc46f
Samet Yatmaz <sametytmz@gmail.com>
2021-08-01 20:49:35 +0300

    intro auto layout was developed.

---
+++ Intro Auto Layout/Intro Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intro Auto Layout
+//
+//  Created by Samet Yatmaz on 1.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
