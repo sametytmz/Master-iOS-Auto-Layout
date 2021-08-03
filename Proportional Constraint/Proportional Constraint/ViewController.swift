5f4eecdaddb1ce5898172f4ace57a97af8b366e9
Samet Yatmaz <sametytmz@gmail.com>
2021-08-03 12:07:49 +0300

    Proportional Constraints was developed.

---
+++ Proportional Constraint/Proportional Constraint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proportional Constraint
+//
+//  Created by Samet Yatmaz on 3.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
